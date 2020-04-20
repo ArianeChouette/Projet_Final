966a0f677ac685138a6295dde1fc858d60f4f9e4
ArianeChouette <61501682+ArianeChouette@users.noreply.github.com>
2020-04-20 10:31:15 +0200

    Create charte.php

---
+++ charte.php
@@ -0,0 +1,20 @@
+<?php
+
+    $title = "Charte";
+    include("include/init.php");
+    include("include/header.php");
+
+?>
+
+<ol id="Ch">
+  <p style="margin-bottom: 120px">En acceptant la charte je <span id="N">m'engage à respecter</span> les règles suivantes :</p><br><br><br>
+  <li style="margin-bottom: 105px">Pour toute offre faite sur un article, vous vous <span id="N">engagez </span> à verser la somme indiquée en cas d'acceptation du vendeur.</li>
+  <li style="margin-bottom: 105px">Pour un achat en "Meilleure offre" il vous est possible de négocier <span id="N"> 5 fois </span>le prix. Après cela il ne vous sera plus possible de le faire.</li>
+  <li style="margin-bottom: 110px">Tout objet mis en vente <span id="N">justifie</span> sa valeur et ne peut être contrefait.</li>
+</ol>
+
+
+
+<?php
+  include("include/footer.php");
+ ?>
